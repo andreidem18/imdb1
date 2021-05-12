3996eaefc50f0d94368791a3ccf6ba531253eadf
Andrés Rojas <andres.02rojas@gmail.com>
2021-05-12 18:26:49 -0500

    whit aout verifyToken

--- src/routes/directors.js
+++ src/routes/directors.js
@@ -1,15 +1,14 @@
 const {get, getAll, create, deleteDirector, update, updatePhoto} = require('../controllers/directors.controllers');
-const verifyToken = require('./verifyToken');
 const {Router} = require('express');
 const multer = require('multer');
 const mimetype = require('mime-types');
 const route = Router();
 
-route.get("/directors/:id",verifyToken, get);
-route.get("/directors",verifyToken, getAll);
-route.post("/directors",verifyToken, create);
-route.delete("/directors/:id",verifyToken, deleteDirector);
-route.put("/directors/:id",verifyToken, update);
+route.get("/directors/:id", get);
+route.get("/directors", getAll);
+route.post("/directors", create);
+route.delete("/directors/:id", deleteDirector);
+route.put("/directors/:id", update);
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,8 @@ const {get, getAll, create, deleteUser, update, login} = require('../controllers
 const {Router} = require('express');
 const route = Router();
 
-route.get("/users/:id", verifyToken, get);
-route.get("/users", verifyToken,  getAll);
+route.get("/users/:id", get);
+route.get("/users", getAll);
 route.post("/users", create);
 route.post("/login", login);
 route.delete("/users/:id", deleteUser);
