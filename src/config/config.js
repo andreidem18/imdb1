dd0a6bf17111651a29177c0a27e681385a3e4168
Andrés Rojas <andres.02rojas@gmail.com>
2021-05-25 21:25:44 -0500

    Create Readme.me

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "gato1231",
-    "database": "imdb",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
@@ -16,12 +16,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    use_env_variable: "DATABASE_URL",
-    dialectOptions: {
-      ssl:{
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+      "production": {
+      "username": "root",
+      "password": null,
+      "database": "database_production",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
   }
 }
